6876d132b01a4fb7a3150c211e38ee37489cb68b
AtalsKim <644516977@qq.com>
2019-09-16 16:29:24 +0800

    还是不行，准备更改一下结构大改

--- lstmsintest_0912_forshort.py
+++ lstmsintest_0912_forshort.py
@@ -139,164 +139,177 @@ class LSTMpred(nn.Module):
         self.input_dim = input_size
         self.hidden_dim = hidden_dim
         self.num_layer = num_layer
-        self.hidden = self.init_hidden()
+        self.batchsize = 1
+        # self.hidden = self.init_hidden()
         # 数据归一化操作
         # self.bn1 = nn.BatchNorm1d(num_features=320)
         # 增加DROPout 避免过拟合
         self.lstm = nn.LSTM(input_size, hidden_dim, num_layer, dropout=0.5)
-        # out 为什么是1？？？
-        # self.hidden2out = nn.Linear(hidden_dim, 1)
+        # outfeature = 1
+        self.hidden2out = nn.Linear(self.hidden_dim, 1)
 
 
     # 第一个求导应该不用的吧
     def init_hidden(self):
-        return (Variable(torch.zeros(self.num_layer, 1, self.hidden_dim)).cuda(),
-                Variable(torch.zeros(self.num_layer, 1, self.hidden_dim)).cuda())
+        return (Variable(torch.zeros(self.num_layer, self.batchsize, self.hidden_dim)).cuda(),
+                Variable(torch.zeros(self.num_layer, self.batchsize, self.hidden_dim)).cuda())
 
     def forward(self, seq):
         # 三个句子，10个单词，1000
-        # input >>> [seq, batch, vec]
+
         # hc维度应该是 [层数，batch, hiddensize]
         # out 维度应该是[单词, batch, hiddensize]
         # lstm_out, self.hidden = self.lstm(seq.view(len(seq), 1, -1), self.hidden)
         # seq =1  batch 1 vec 200
-        lstm_out, self.hidden = self.lstm(seq.view(1, 1, len(seq)), self.hidden)
-        # outdat = self.hidden2out(lstm_out.view(len(seq),-1))
-        return lstm_out
+
+        # vecinput 行数据的个数
+        # input >>> [seq_len, batchsize, input_size]
+        # out >>> [seq_len, bathchsize, hiddenlayernum]
+        # h,c >>> [层数，batchsize, hiddensize]
+        lstm_out, self.hidden = self.lstm(seq.view(len(seq), self.batchsize, -1), self.hidden)
+        # 是不是多对一的话留下最后结果
+        outdat = self.hidden2out(lstm_out[-1].view(self.batchsize, -1))
+        return outdat.view(-1)
+        # outdat = self.hidden2out(lstm_out.view(len(seq), -1))
+        # return outdat
 
 
 
-###
-# 算是主函数
-###
+def main():
+
+    TESTMOD = False
+    NEUNUM = 16
+    NLAYER = 2
+    testlen = 32
+    step = 4
+    num_epochs = 500
+    # inputsize 这个应该是指特征的维度，所以是1
+    model = LSTMpred(1, NEUNUM, NLAYER).to('cuda')
+    # 改用Adam
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    loss_function = nn.MSELoss()
+    # 配置输入batch
+    # xlpath = r'excelTest37000.xlsx'
+    # xlpath = r'Prep_DATA0915.xlsx'
+    xlpath = r'SINETEST1000.xls'
+    # 12500~ 15000 有坏值
+    x, y = loaddata(xlpath, length = -1, start=1)
+    trainlen = round(len(x)*0.7)
+    # 后1000 留下测试
+
+    dat = trainDataGen(x[:trainlen], y[:trainlen], testlen, step=step)
+    # 测试集
+    testdata = trainDataGen(x[trainlen:], y[trainlen:], testlen, step=step)
+    rloss = []
+    Tloss_list = []
+    print("NNumber:{}, TestLen:{}, Epochs:{}".format(NEUNUM, testlen, num_epochs))
+    # ship_train_loader = DataLoader(ship_train_dataset, batch_size=16, num_workers=4,
+    #                                shuffle=False, **kwargs)
+
+    # 测试模块， 会假死
+    if TESTMOD:
+        tlen = 10000
+        tstart = 10000
+        x = x[tstart:tstart+tlen]
+        y = y[tstart:tstart+tlen]
+        trueDat, predDat = postPlot(model, x, y)
+        save2excel([trueDat, predDat], xlname='Pred_Truth2.xls')
+        quit()
+
+# seqn, feature1,
+    # batch 一次送多少个数据
 
 
 
+    print("Epoch Start...")
+    for epoch in range(num_epochs):
+        # print(epoch, end='')
+        running_loss = 0.0
+        for seq, outs in dat:
+            seq = ToVariable(seq).cuda()
+            # 1,1,teslen
+            seq = seq.view(len(seq), 1)
+            outs = ToVariable(outs).cuda()
+            outs = outs.view(len(outs), 1)
+            # 由于输入的时array 改为 a x 1 的格式
+            # 修改完之后有明显降低
+            #outs = torch.from_numpy(np.array([outs]))
 
-TESTMOD = False
-NEUNUM = 128
-NLAYER = 2
-testlen = 200
-step = 4
-# inputsize 注意
-model = LSTMpred(testlen, NEUNUM, NLAYER).to('cuda')
-# optimizer = optim.SGD(model.parameters(), lr=0.01)
-# 改用Adam
-optimizer = optim.Adam(model.parameters(), lr=0.0002)
-loss_function = nn.MSELoss()
-# 配置输入batch
-# xlpath = r'excelTest37000.xlsx'
-# xlpath = r'Prep_DATA0915.xlsx'
-xlpath = r'SINETEST1000.xls'
-# 12500~ 15000 有坏值
-x, y = loaddata(xlpath, length = -1, start=1)
-trainlen = round(len(x)*0.7)
-# 后1000 留下测试
-# 分段长度
-dat = trainDataGen(x[:trainlen], y[:trainlen], testlen, step=step)
-# 测试集
-testdata = trainDataGen(x[trainlen:], y[trainlen:], testlen, step=step)
-num_epochs = 500
-rloss = []
-Tloss_list = []
-print("NNumber:{}, TestLen:{}, Epochs:{}".format(NEUNUM, testlen, num_epochs))
+            # 清除网络状态
+            optimizer.zero_grad()
+            # 重新初始化隐藏层数据
+            model.hidden = model.init_hidden()
 
 
-# 测试模块， 会假死
-if TESTMOD:
-    tlen = 10000
-    tstart = 10000
-    x = x[tstart:tstart+tlen]
-    y = y[tstart:tstart+tlen]
-    trueDat, predDat = postPlot(model, x, y)
-    save2excel([trueDat, predDat], xlname='Pred_Truth2.xls')
-    quit()
+            modout = model(seq).cuda()
+            loss = loss_function(modout, outs)
+            # 反向传播求梯度
+            loss.backward()
+            # 更新参数
+            optimizer.step()
+            # 放里头一直更新
+            # statistics
+            # running_loss += loss.item()
+            running_loss = loss.item()
+        # 临时绘图
+
+        # 测试区段数据
+        Testloss = 0
+        # 测试集长度
+        testdatalen = len(testdata)
+        for t1 in testdata:
+            testx = ToVariable(t1[0]).view(len(t1[0]),1).cuda()
+            # model input 必须是 [xxx,1]
+            predDat = model(testx).data.cpu()
+            predDat = predDat.numpy()
+            trueDat = t1[1]
+            # 测试误差
+            Tloss_truDat = np.array(trueDat)
+            Testloss0 = loss_function(ToVariable(predDat), ToVariable(trueDat).view(len(trueDat), 1))
+            Testloss += Testloss0
+        Testloss = Testloss/testdatalen
+        # 简单绘图
+        # simplot(trueDat, predDat)
+        rloss.append(running_loss)
+        Tloss_list.append(Testloss)
+        print(' Epoch[{}/{}], loss:{:.6f}， Tloss:{:.6f}'.format(epoch, num_epochs, running_loss,Testloss))
 
 
-print("Epoch Start...")
-for epoch in range(num_epochs):
-    # print(epoch, end='')
-    running_loss = 0.0
-    for seq, outs in dat:
-        seq = ToVariable(seq).cuda()
-        # 1,1,teslen
-        seq = seq.view(len(seq), 1)
-        outs = ToVariable(outs).cuda()
-        outs = outs.view(len(outs), 1)
-
-        # 由于输入的时array 改为 a x 1 的格式
-        # 修改完之后有明显降低
-        #outs = torch.from_numpy(np.array([outs]))
-
-        # 清除网络状态
-        optimizer.zero_grad()
-        model.hidden = model.init_hidden()
-        modout = model(seq).cuda()
-        loss = loss_function(modout, outs)
-        # 反向传播求梯度
-        loss.backward()
-        # 更新参数
-        optimizer.step()
-        # 放里头一直更新
-        # statistics
-        # running_loss += loss.item()
-        running_loss = loss.item()
-    # 临时绘图
-
-    # 测试区段数据
-    Testloss = 0
-    # 测试集长度
-    testdatalen = len(testdata)
-    for t1 in testdata:
-        testx = ToVariable(t1[0]).view(len(t1[0]),1).cuda()
-        # model input 必须是 [xxx,1]
-        predDat = model(testx).data.cpu()
-        predDat = predDat.numpy()
-        trueDat = t1[1]
-        # 测试误差
-        Tloss_truDat = np.array(trueDat)
-        Testloss0 = loss_function(ToVariable(predDat), ToVariable(trueDat).view(len(trueDat), 1))
-        Testloss += Testloss0
-    Testloss = Testloss/testdatalen
-    # 简单绘图
-    # simplot(trueDat, predDat)
-    rloss.append(running_loss)
-    Tloss_list.append(Testloss)
-    print(' Epoch[{}/{}], loss:{:.6f}， Tloss:{:.6f}'.format(epoch, num_epochs, running_loss,Testloss))
+        # # 计算与测试集的误差
+        # predDat = model(ToVariable(x[-testlen:]).cuda()).data.cpu()
+        # predDat = np.array(predDat)
+        # trueDat = y[-testlen:]
+        # Testloss = loss_function(modout, outs)
 
 
-    # # 计算与测试集的误差
-    # predDat = model(ToVariable(x[-testlen:]).cuda()).data.cpu()
-    # predDat = np.array(predDat)
-    # trueDat = y[-testlen:]
-    # Testloss = loss_function(modout, outs)
+        # 保存模型参数
+        # 保存模型
+        if epoch % 10 == 0:
+            pklname = 'param_N{}_Len{}_Ep{}_St{}0916.pkl'.format(NEUNUM,testlen,num_epochs,step)
+            # 保存误差列表
+            try:
+                save2excel([rloss, Tloss_list], xlname='LossHistr1t20912.xls')
+            except:
+                print('Loss saving failed.')
+            # save2excel([Tloss_list], xlname='TestLossHist0912.xls')
+            torch.save(model.state_dict(), pklname)
+            print("> Parameters have been saved.")
 
 
-    # 保存模型参数
-    # 保存模型
-    if epoch % 10 == 0:
-        pklname = 'param_N{}_Len{}_Ep{}_St{}0916.pkl'.format(NEUNUM,testlen,num_epochs,step)
-        # 保存误差列表
-        try:
-            save2excel([rloss, Tloss_list], xlname='LossHistr1t20912.xls')
-        except:
-            print('Loss saving failed.')
-        # save2excel([Tloss_list], xlname='TestLossHist0912.xls')
-        torch.save(model.state_dict(), pklname)
-        print("> Parameters have been saved.")
-              
+
+    # 最终测试
+    predDat = model(ToVariable(x[-testlen:]).cuda()).data.cpu()
+    predDat = np.array(predDat)
+    trueDat = y[-testlen:]
+    fig = plt.figure()
+    plt.plot(trueDat, label= 'Turedata')
+    plt.plot(predDat, label= 'Predict', alpha=0.4)
+    plt.legend()
+    plt.show()
+    # 保存至EXCEL
+    # save2excel([trueDat, predDat], xlname='Pred_Truth2.xls')
+    save2excel([trueDat, predDat], xlname='Final_lstm_TestData0912.xls')
 
 
-# 最终测试
-predDat = model(ToVariable(x[-testlen:]).cuda()).data.cpu()
-predDat = np.array(predDat)
-trueDat = y[-testlen:]
-
-fig = plt.figure()
-plt.plot(trueDat, label= 'Turedata')
-plt.plot(predDat, label= 'Predict', alpha=0.4)
-plt.legend()
-plt.show()
-# 保存至EXCEL
-# save2excel([trueDat, predDat], xlname='Pred_Truth2.xls')
-save2excel([trueDat, predDat], xlname='Final_lstm_TestData0912.xls')
+if __name__ == '__main__':
+    main()
